7df949a323c98b2206c81563f7c8f0d1853b8d0e
madsmccannot <153859615+madsmccannot@users.noreply.github.com>
2026-05-14 14:38:20 +0100

    Improve hover effect abd add function to create grid

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".grid-btn");
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+function createGrid(size) {
+  container.innerHTML = "";
+
+  const squareSize = 640 / size;
+
+  for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+  
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "beige";
+    });
+
+    container.appendChild(square);
+  }
 }
+
+button.addEventListener("click", () => {
+  let userInput = prompt("Enter number of squares per side (max 100)");
+
+  userInput = Number(userInput);
+
+  if (userInput > 0 && userInput <= 100) {
+    createGrid(userInput);
+  }
+});
+
+createGrid(16);
